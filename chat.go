ef33a0bfdc74f80695405ac0a190303f0c4ab6f5
Stephen Mahon <stemahon@gmail.com>
2021-11-29 23:48:11 -0600

    fixed errors and added flags

--- chat.go
+++ chat.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/ably/ably-go/ably"
 	"github.com/joho/godotenv"
@@ -15,31 +14,22 @@ func main() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatalf("error loading .env file:", err)
+		log.Fatalf("error loading .env file: %v", err)
 	}
 
-	var username string
-
-	// If no username specified, ask for one
-	if len(os.Args) < 2 {
-		fmt.Println("Type your username")
-		reader := bufio.NewReader(os.Stdin)
-		username, _ = reader.ReadString('\n')
-		username = strings.Replace(username, "\n", "", -1)
-	} else {
-		username = os.Args[1]
-	}
+	username := flag.String("u", "Steve", "username")
+	flag.Parse()
 
 	// Connect to Ably using the API key and ClientID specified above
 	client, err := ably.NewRealtime(
 		ably.WithKey(os.Getenv("ABLY_KEY")),
 		// ably.WithEchoMessages(true), // Uncomment to stop messages you send from being sent back
-		ably.WithClientID(username))
+		ably.WithClientID(*username))
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("You can now send messages!")
+	fmt.Printf("%s, you can now send messages!\n", client.Auth.ClientID())
 	_ = client
 
 }
